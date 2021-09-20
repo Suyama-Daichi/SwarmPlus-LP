c863d524709bad1f253c4f1f14083ae543c32cbe
daichi.suyama <symdit@gmail.com>
2021-09-20 16:30:18 +0900

    feat: GA設定

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,7 +17,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "YOUR_GOOGLE_ANALYTICS_TRACKING_ID",
+        trackingId: "G-P7HRM2QTPC",
       },
     },
     {
